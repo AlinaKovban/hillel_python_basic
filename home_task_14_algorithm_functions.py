505190e21c4d4d32e44e470eb2d60c5e1197a5ae
AlinaKovban <127793085+AlinaKovban@users.noreply.github.com>
2023-03-22 22:18:28 +0200

    Update home_task_14_algorithm_functions.py

--- home_task_14_algorithm_functions.py
+++ home_task_14_algorithm_functions.py
@@ -9,28 +9,28 @@ def func():
         
 def func2(value):
     if value.startswith('-') and value[1:].isdigit():
-        print(f'Ви ввели целое отрицательное {value}')
+        return f'Ви ввели целое отрицательное {value}'
     elif value.isdigit():
         if int(value) == 0:
-            print(f'Ви ввели ноль {value}')
+            return f'Ви ввели ноль {value}'
         else:
             int(value)
-            print(f'Ви ввели целое число {value}')
+            return f'Ви ввели целое число {value}'
     elif value.startswith('-') and '.' in value or ',' in value:
         try:
             float_value = value.replace(',', '.')
             float(float_value)
-            print(f'Ви ввели отрицательное дробне число {float_value}')
+            return f'Ви ввели отрицательное дробне число {float_value}'
         except ValueError:
-            print(f'Ви ввели не число {value}')
+            return f'Ви ввели не число {value}'
     elif '.' in value or ',' in value and value.replace(',', '').isdigit() or value.replace('.', '').isdigit():
         try:
             float(value)
-            print(f'Ви ввели дробне число {value}')
+            return f'Ви ввели дробне число {value}'
         except ValueError:
-            print(f'Ви ввели не число {value}')
+            return f'Ви ввели не число {value}'
     else:
-        print(f'Ви ввели не число {value}')
+        return f'Ви ввели не число {value}'
              
 
 func()
